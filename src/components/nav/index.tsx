7f1cb7c822f3f1854b3e848087605c0f8e93a330
wanger <1206180097@qq.com>
2022-03-04 01:51:51 +0800

    start

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -1,19 +1,9 @@
 import S from './index.module.scss';
-import { navActions } from './actions';
 
 export function Nav() {
   return (
     <div className={S.nav}>
-      {
-        navActions.map(action => {
-          return (
-            <div className={S.wrap} key={action.name}>
-              <img src={action.icon} />
-              <span>{action.name}</span>
-            </div>
-          )
-        })
-      }
+      nav todo
     </div>
   )
 }
